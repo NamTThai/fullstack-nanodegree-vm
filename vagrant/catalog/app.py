45075b1e49f0675bf7f72075d82b5d9171399f11
Nam Thai <nthai@ciosolutions.com>
2015-11-16 22:42:16 -0800

    feat: rss

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, Type, Pokemon
 import urllib2
 import re
+from datetime import datetime
 
 app = Flask(__name__, static_url_path='')
 
@@ -12,21 +13,6 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
-
-@app.route('/Types/<int:Type_id>/menu/JSON')
-def TypeMenuJSON(Type_id):
-    Type = session.query(Type).filter_by(id=Type_id).one()
-    items = session.query(Pokemon).filter_by(
-        Type_id=Type_id).all()
-    return jsonify(Pokemons=[i.serialize for i in items])
-
-
-# ADD JSON ENDPOINT HERE
-@app.route('/Types/<int:Type_id>/menu/<int:menu_id>/JSON')
-def PokemonJSON(Type_id, menu_id):
-    Pokemon = session.query(Pokemon).filter_by(id=menu_id).one()
-    return jsonify(Pokemon=Pokemon.serialize)
 
 
 @app.route('/')
@@ -96,69 +82,32 @@ def modify():
 
 
 @app.route('/v1/pokemon')
-def pokemonInfoV1():
+def pokemonInfo():
     pokemonId = request.args.get('id')
     if pokemonId is None:
-        return jsonify(error="No pokemon ID specified"), 404
+        return jsonify(message="No pokemon ID specified"), 404
     else:
         pokemons = session.query(Pokemon).filter_by(id=pokemonId).all()
         if (len(pokemons) != 1):
-            return jsonify(error="No pokemon ID specified"), 404
+            return jsonify(message="Nothing found"), 404
         else:
             return jsonify(pokemon=pokemons[0].getJSON())
 
 
 @app.route('/v1/types')
-def typesV1():
+def types():
     types = session.query(Type).all()
     return jsonify(types=[t.name for t in types])
 
 
-@app.route('/Types/<int:Type_id>/new', methods=['GET', 'POST'])
-def newPokemon(Type_id):
-
-    if request.method == 'POST':
-        newItem = Pokemon(name=request.form['name'], description=request.form[
-                           'description'], price=request.form['price'], course=request.form['course'], Type_id=Type_id)
-        session.add(newItem)
-        session.commit()
-        return redirect(url_for('TypeMenu', Type_id=Type_id))
-    else:
-        return render_template('newPokemon.html', Type_id=Type_id)
-
-
-@app.route('/Types/<int:Type_id>/<int:menu_id>/edit',
-           methods=['GET', 'POST'])
-def editPokemon(Type_id, menu_id):
-    editedItem = session.query(Pokemon).filter_by(id=menu_id).one()
-    if request.method == 'POST':
-        if request.form['name']:
-            editedItem.name = request.form['name']
-        if request.form['description']:
-            editedItem.description = request.form['name']
-        if request.form['price']:
-            editedItem.price = request.form['price']
-        if request.form['course']:
-            editedItem.course = request.form['course']
-        session.add(editedItem)
-        session.commit()
-        return redirect(url_for('TypeMenu', Type_id=Type_id))
-    else:
-
-        return render_template(
-            'editPokemon.html', Type_id=Type_id, menu_id=menu_id, item=editedItem)
-
-
-@app.route('/Types/<int:Type_id>/<int:menu_id>/delete',
-           methods=['GET', 'POST'])
-def deletePokemon(Type_id, menu_id):
-    itemToDelete = session.query(Pokemon).filter_by(id=menu_id).one()
-    if request.method == 'POST':
-        session.delete(itemToDelete)
-        session.commit()
-        return redirect(url_for('TypeMenu', Type_id=Type_id))
-    else:
-        return render_template('deleteconfirmation.html', item=itemToDelete)
+@app.route('/rss')
+def latestEntriesRss():
+    now = datetime.now()
+    latestEntries = session.query(Pokemon).order_by(desc(Pokemon.date_entered)).limit(20)
+    rss = render_template('rss.xml', lastBuildDate=now, entries=latestEntries)
+    response = make_response(rss)
+    response.headers["Content-Type"] = "application/xml"
+    return response
 
 
 if __name__ == '__main__':
